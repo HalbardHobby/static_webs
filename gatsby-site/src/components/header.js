25cf827c179e82cf00d9c087db58b45a2fab2e72
Alejandro Espinosa <HalbardHobby@users.noreply.github.com>
2019-12-07 16:39:59 -0500

    some graphql

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -1,42 +1,56 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import headerStyles from './header.module.scss'
 
-const Header = () => (
-  <header className={headerStyles.header}>
-    <h1>
-      <Link to='/' className={headerStyles.title}>Sample Site</Link>
-    </h1>
-    <nav>
-      <ul className={headerStyles.navList}>
-        <li>
-          <Link to='/' className={headerStyles.navItem}
-            activeClassName={headerStyles.activeNavItem}>
-            Home
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  
+  return (
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to='/' className={headerStyles.title}>
+          {data.site.siteMetadata.title}
+        </Link>
+      </h1>
+      <nav>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link to='/' className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}>
+              Home
           </Link>
-        </li>
-        <li>
-          <Link to='/about' className={headerStyles.navItem}
-            activeClassName={headerStyles.activeNavItem}>
-            About
+          </li>
+          <li>
+            <Link to='/about' className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}>
+              About
           </Link>
-        </li>
-        <li>
-          <Link to='/blog' className={headerStyles.navItem}
-            activeClassName={headerStyles.activeNavItem}>
-            Blog
+          </li>
+          <li>
+            <Link to='/blog' className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}>
+              Blog
           </Link>
-        </li>
-        <li>
-          <Link to='/contact' className={headerStyles.navItem}
-            activeClassName={headerStyles.activeNavItem}>
-            Contact
+          </li>
+          <li>
+            <Link to='/contact' className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}>
+              Contact
           </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Header
