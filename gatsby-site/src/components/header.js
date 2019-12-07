dc9c302b599cd355495e40a696154a1809f2a4dd
Alejandro Espinosa <HalbardHobby@users.noreply.github.com>
2019-12-07 16:12:58 -0500

    navigation and styling

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from './header.module.scss'
+
 const Header = () => (
-  <header>
-    <h1>Sample Site</h1>
+  <header className={headerStyles.header}>
+    <h1>
+      <Link to='/' className={headerStyles.title}>Sample Site</Link>
+    </h1>
     <nav>
-      <ul>
-        <li><Link to='/'>Home </Link></li>
-        <li><Link to='/about'>About </Link></li>
-        <li><Link to='/blog'>Blog </Link></li>
-        <li><Link to='/contact'>Contact </Link></li>
+      <ul className={headerStyles.navList}>
+        <li>
+          <Link to='/' className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/about' className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='/blog' className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}>
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link to='/contact' className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}>
+            Contact
+          </Link>
+        </li>
       </ul>
     </nav>
   </header>
